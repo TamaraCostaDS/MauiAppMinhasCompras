7efb1138a255a350040690fd69ac9cccd08b252d
Tamara Costa <tamara_acds@hotmail.com>
2026-03-30 01:57:15 -0300

    Validação da inserção dos produtos

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,15 +4,28 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
-        public string Descricao { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+                _descricao = value;
+            }
+        }
 
         public double Quantidade { get; set; }
 
-        public double Preco { get; set; } 
+        public double Preco { get; set; }
 
-        public double Total { get => Quantidade * Preco; } 
+        public double Total { get => Quantidade * Preco; }
     }
 }
